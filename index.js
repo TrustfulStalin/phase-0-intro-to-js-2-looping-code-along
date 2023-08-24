ff6bef96ccadab6b1ed4931d55de2295b8e794be
Tevon Talbert <TevonTalbert@hotmail.com>
2023-08-24 16:53:45 -0500

    adding

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function writeCards(writeCards) {
+    for (let i = 0; i < writeCards.length; i++) {
+        writeCards[0] = "Thank you, Guadalupe, for the wonderful surprise gift!";
+        writeCards[1] = "Thank you, Ollie, for the wonderful surprise gift!";
+        writeCards[2] = "Thank you, Aki, for the wonderful surprise gift!";
+        return writeCards;
+        debugger;
+    }
+}
+
+function countDown(number)
+{
+    
+        while (number >= 0) {
+            console.log(number);
+            number--;
+        }
+    
+
+   
+}
+
+countDown(10);
